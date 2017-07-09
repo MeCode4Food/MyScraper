f5546db231336233b6ef6ff97c531f10aff84de6
Chick <cheecken0@hotmail.com>
2017-07-09 22:19:49 +0800

    Add project files.

---
+++ MyScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
