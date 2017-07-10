4fce2d84f95a7fd12f8f556032ddbcea9d3f9340
Chick <cheecken0@hotmail.com>
2017-07-10 09:06:36 +0800

    Origin

--- MyScraper/Program.cs
+++ MyScraper/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using HtmlAgilityPack;
 
 namespace MyScraper
-{
+{ 
     class Program
     {
+        public static string websiteURL;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! MyScraper v0.1 by Chick");
+
+            Console.WriteLine("Input url to parse data");
+            Console.Write("URL: ");
+            Program.websiteURL = Console.ReadLine();
+
+            HtmlWeb 
+        }
+
+        static void Start()
+        {
+            
+
+
         }
     }
 }
